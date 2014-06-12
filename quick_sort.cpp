23e96672fdcd2eb7d0870601ce274a6ac46b2058
hljyunxi <hljyunxi@126.com>
2014-06-12 15:45:05 +0800

    fix quick_sort bug

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,62 +1,43 @@
-#include <iostream.h>
+#include <iostream>
 #include <stdlib.h>
+#include <stdio.h>
 
 template <class T>
-void quick_sort_1(T a[], int low, int high){
+void swap(T * a, T * b){
+    T t = *a;
+    *a = *b;
+    *b = t;
+}
+
+template <class T>
+void quick_sort(T* a, int low, int high){
 
     if(!(low < high))
         return ;
 
-    //partition
-    int p = rand()%(high-low) + low;
-    T cmp_data = a[p];
-
-    int i = low, j = high - 1;
+    int i = low, j = high;
+    T cmp_data = a[low];
     while(i < j){
-        while(i < j && a[j] > cmp_data)
+        while(i < j && a[j] >= cmp_data)
             j--;
+        if(i < j)
+            a[i] = a[j];
 
         while(i < j && a[i] < cmp_data)
             i++;
-
         if(i < j)
-            swap(a[i++], a[j--]);
-    }
-
-    quick_sort_1(a, low, i);
-    quick_sort_1(a, i+1, high);
-}
-
-template <class T>
-void quick_sort_2(T a[], int low, int high){
-
-    //partition
-    int p = rand()%(high-low) + low;
-    T cmp_data = a[p];
-
-    int i = low, j = high - 1;
-    while(i < j){
-        while(i < j && a[j] > cmp_data)
-            j--;
-        if(i < j)
-            a[i++] = a[j];
-
-        while(i < j && a[i] < tmp_data)
-            i++;
-        
-        if(i < j)
-            a[j--] = a[i]
+            a[j] = a[i];
     }
     a[i] = cmp_data;
-    quick_sort_2(a, low, i);
-    quick_sort_2(a, i+1, high);
+    quick_sort(a, low, i);
+    quick_sort(a, i + 1, high);
 }
 
-template <class T>
-void qucik_sort(T a[], int size){
-    quick_sort(a, 0, size);
-}
 
 int main(){
+    int a[] = {1, 7, 4, 3, 5, 8, 9, 12, 2, 15};
+    quick_sort((int * )a, 0, 9);
+    for(int i = 0; i < 10; i++)
+        printf("%d->", a[i]);
     return 0;
 }
